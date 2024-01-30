ee8826b22e4bf6bbccc0f88cf691e3c3fa880552
gpaul988 <gpaul988gmail.com>
2024-01-30 23:30:57 +0100

    most recent used cache added

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Most Recently Used caching module.
+# Graham S. Paul - 4-mru_cache.py
+"""This displays the Most Recently Used caching module.
 """
 from collections import OrderedDict
 
@@ -7,18 +8,18 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with an MRU
+    """Shows an object that allows saving  and
+    getting back items from a dictionary with an MRU
     removal mechanism when the limit is reached.
     """
     def __init__(self):
-        """Initializes the cache.
+        """Boots the cache.
         """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item in the cache.
+        """includes an item in the cache.
         """
         if key is None or item is None:
             return
@@ -32,7 +33,7 @@ class MRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieves an item by key.
+        """gets back an item by key.
         """
         if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
